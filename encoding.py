2fcad29e0c2ee75eb273da42aa62acdc303eae13
Alaa  Jamal Abdalqader <162041062+AlaaJam@users.noreply.github.com>
2024-04-16 09:40:15 +0300

    edit

---
+++ encoding.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def one_hot_encoding(data, columns):
+    return pd.get_dummies(data, columns=columns)
+
+def label_encoding(data, column):
+    labels = data[column].astype('category').cat.codes
+    return labels
